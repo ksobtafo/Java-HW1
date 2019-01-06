60fbc795da9e9377c80533b0f67478cf875aced9
Karen Sobtafo <43358183+ksobtafo@users.noreply.github.com>
2019-01-06 15:25:09 -0800

    Create EX1_6(1).java

---
+++ EX1_6(1).java
@@ -0,0 +1,12 @@
+//Design a class to support Date objects (day, month, year). 
+//Your date class should be smart (e.g. I should not be able to create a date for the 31st of February).
+// In addition to get/set functions, your class should be able to return today’s Date (static function), 
+//calculate the difference (in days) between two date objects, 
+//and create a new date n days in the future of the current date object.
+package pp;
+public class Date {
+    int day;
+    int month;
+    int year;
+    
+}
