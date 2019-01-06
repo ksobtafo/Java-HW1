0e140d9880ec514cac3a4e6954b8f2e01f7b8fdc
Karen Sobtafo <43358183+ksobtafo@users.noreply.github.com>
2019-01-06 14:39:44 -0800

    Create EX1_5(1).java

---
+++ EX1_5(1).java
@@ -0,0 +1,10 @@
+//Design a class to manage employees at a company. An Employee has a unique ID, a name, a hire date, their current position at the company, and who they report to.
+// Give the UML for your class.
+private class Employee {
+    string name;
+    string HireDate;
+    string PositionAtCompany;
+    string ReportTo; 
+    int ID; 
+ }
+    
