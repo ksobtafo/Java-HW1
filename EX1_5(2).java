70a2de60158891e327ca37e3a59587693a673297
Karen Sobtafo <43358183+ksobtafo@users.noreply.github.com>
2019-01-07 12:39:46 -0800

    Update EX1_5(2).java

--- EX1_5(2).java
+++ EX1_5(2).java
@@ -1,15 +1,15 @@
 
-package p; 
 public class EmployeeTest{
+  
+ public static void main(String[]args){
 Employee Person1 = new Employee(); // person object 
-
+    
 Person1.Name =" Kent Jones";
 Person1.HireDate = "June 10th";
 Person1.PositionAtCompany = "Teacher";
 Person1.ID = 1726070;
 Person1.ReportTo = " Beck Taylor";
-System.out.println("The first Employee's name is "+Person1.Name + " this person got hired " +Person1.HireDate+
-                     "this person is a " +Person1.PositionAtCompany+ " this person's ID  number is"+ Person1.ID+
-                     " and this person reports to " + Person1.ReportTo );
+System.out.println(Person1.toString());
 
+}
 }
