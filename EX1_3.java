7895559cf395ad8890270b8bf97b2903903a64d9
Karen Sobtafo <43358183+ksobtafo@users.noreply.github.com>
2019-01-07 15:01:28 -0800

    Update EX1_3.java

--- EX1_3.java
+++ EX1_3.java
@@ -10,64 +10,20 @@ public class StringOrInt
      int num;
     num = input.nextInt();
     //store input into a string
-    
-    string word = ""; 
-    int num2 =0;
-    while (num>0)
-    {
-        int digit = num % 10;
-        if (digit==0)
-        {
-            word += "0";
-        }
-        else if (digit ==1)
-        {
-            word += "1";
-        }
-        else if (digit ==2)
-        {
-            word += "2";
-        }
-        else if (digit ==3)
-        {
-            word += "3";
-        }
-        else if (digit ==4)
-        {
-            word += "4";
-        }
-        else if (digit ==5)
-        {
-            word += "5";
-        }
-        else if (digit ==6)
-        {
-            word += "6";
-        }
-        else if (digit ==7)
-        {
-            word += "7";
-        }
-        else if (digit ==8)
-        {
-            word += "8";
-        }
-        else if (digit ==9)
-        {
-            word += "9";
-        }
-    }
+    String word=input.nextLine();
+    //int num2 =0;
     //if the fist character of the string is a letter than convert it into an interger
     if (Character.isDigit(word.charAt(0))==true)
     {
     //convert sting to interger
-    for (int i; i<num2.lengh(); i++)
+    for (int i; i<num.lengh(); i++)
         {
-            num2 = num2*10 + num2.charAt(i)-'0';
+            num = num*10 + num.charAt(i)-'0';
         }
     }
     else 
     {
     System.out.printf(" You did not input a number!  "); 
     }
+}
 }
