bc5d2147a5a90548625734c281d53b21240cd0d8
Karen Sobtafo <43358183+ksobtafo@users.noreply.github.com>
2019-01-07 16:30:15 -0800

    Update EX1_6(2).java

--- EX1_6(2).java
+++ EX1_6(2).java
@@ -1,4 +1,38 @@
-package pp;
-public class DateTest{
+//Design a class to support Date objects (day, month, year). 
+//Your date class should be smart (e.g. I should not be able to create a date for the 31st of February).
+// In addition to get/set functions, your class should be able to return today’s Date (static function), 
+//calculate the difference (in days) between two date objects, 
+//and create a new date n days in the future of the current date object.
+
+public class DateTest {
+    public static void main(String[] args) {
+
+        public void setDay(int name)
+        {
+            Name = name;
+        }
+        public int GetDay()
+        {
+            Return Day;
+        }
+        public void SetMonth (int month) 
+        {
+            Month = month;
+        }
+        public String GetMonth()
+        {
+            return Month;
+        }
+        public void SetYear(int year)
+        {
+            Year = year;
+        }
+        public int GetYear()
+        {
+            return Year;
+        }
+
+    }
+
     
 }
